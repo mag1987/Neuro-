13aa6edf06391e96695e89917ed513cbcff0f58a
mag987 <mag987@list.ru>
2019-02-27 22:17:00 +0700

    temp

--- diplom/Program.cs
+++ diplom/Program.cs
@@ -7,23 +7,8 @@ using System.Threading.Tasks;
 
 namespace diplom
 {
-    public delegate Output<T> ActivationFunction<T>(Input<T>[] inputs);
-    public class Input<T>
-    {
-        public T Value {get; set;}
-        public Input()
-        {
-            Value = default(T);
-        }
-    }
-    public class Output<T>
-    {
-        public T Value { get; set; }
-        public Output()
-        {
-            Value = default(T);
-        }
-    }
+    public delegate T ActivationFunction<T>(T[] inputs);
+    /*
     public class MultiInput<T, Q> where Q : IEnumerable<Input<T>>, IList, new ()
     {
         public Q Inputs { get; set; }
@@ -33,19 +18,28 @@ namespace diplom
         }
 
     }
+    */
+    public class ActivationFunctions<T> where T : Neuron<T>
+    {
+        public static T Sigmoid<T>()
+        {
+            return null;
+        }
+    }
     public class Neuron<T> 
     {
-        public Input<T>[] Inputs { get; set; }
-        public Output<T> Output { get; set; }
-        private ActivationFunction<T> AF;
+        public T[] Inputs { get; set; }
+        public T Output { get; set; }
+        private F _activationFunction;
         public Neuron() : this (10)
         {
 
         }
         public Neuron(int numberOfInputs)
         {
-            Inputs = new Input<T>[numberOfInputs];
-            Output = new Output<T>();
+            Inputs = new T[numberOfInputs];
+            Output = _activationFunction;
+            _activationFunction = ActivationFunction<T>;
         }
     }
     class Program
